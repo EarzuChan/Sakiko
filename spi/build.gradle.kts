c4ac6ec53fa2fc5d2f9198f84c940dfbbef57f0f
Earzu <huascq@163.com>
2024-08-15 11:11:50 +0800

    大刀阔斧地整理项目结构|升级JDK至LTS的21|升级Gradle|微操CI构建脚本

---
+++ spi/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+    id("java-library")
+    kotlin("jvm")
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+    mavenCentral()
+}
+kotlin {
+    jvmToolchain(21)
+}
